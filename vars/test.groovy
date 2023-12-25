cebb20df30de72fec886872685a7ef2ed6760b1c
Derrald Vogt <derrald@dvogt.com>
2023-12-25 09:36:11 -0800

    testing groovy file script

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,10 +1,10 @@
 
 
 
-def call() {
-  echo "Hello, Me."
-}
-
-// stage("Test") {
-//   println("this is the test stage")
+// def call() {
+//   echo "Hello, Me."
 // }
+
+stage("Test") {
+  println("this is the test stage")
+}
